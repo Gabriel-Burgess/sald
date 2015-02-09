e11ecab75f9fcd731db7a4162b1c4d59a5d0658d
Gabriel-Burgess <gburgess56@yahoo.com>
2015-02-09 16:09:01 -0500

    Implemented four functions
    circleCircle, rectangleRectangle, rayCircle, and rayRectangle have been
    implemented.

--- sald/collide.js
+++ sald/collide.js
@@ -1,4 +1,3 @@
-
 /* Circle vs Circle
  * INPUT: two circles specified by position and radius:
  *  c1 = {x:, y:, r:}, c2 = {x:, y:, r:}
@@ -6,8 +5,28 @@
  *  false if c1 and c2 do not intersect
  *  true if c1 and c2 do intersect
  */
-function circleCircle(c1,c2) {
-	//TODO
+function circleCircle(c1,c2)
+{    
+    //Closest distance the circles can be without overlap.
+	var threshold = c1.r + c2.r;
+    
+    var vector2= function()
+    {
+        x : NaN,
+        y : NaN
+    };
+    
+    var temp = new vector2(c1.x - c2.x, c1.y - c2.y);
+    
+    //It is squared so that we don't have to take the square root.
+    var magnitudeSquared = (temp.x * temp.x) + (temp.y * temp.y);
+    
+    //The threshold must be squared since the magnitude is also squared.
+    if(magnitudeSquared < (threshold * threshold))
+    {
+        return true;
+    }
+
 	return false;
 }
 
@@ -18,9 +37,21 @@ function circleCircle(c1,c2) {
  *  false if r1 and r2 do not intersect
  *  true if r1 and r2 do intersect
  */
-function rectangleRectangle(r1, r2) {
-	//TODO
-	return false;
+function rectangleRectangle(r1, r2)
+{
+	//Check to see if they are separated horizontally.
+    if(r1.max.x < r2.min.x || r2.max.x < r1.min.x)
+    {
+        return false;
+    }
+    
+    //Check to see if they are separated vertically.
+    if(r1.max.y < r2.min.y || r2.max.y < r1.min.y)
+    {
+        return false;
+    }
+    
+	return true;
 }
 
 /* Convex vs Convex
@@ -43,8 +74,24 @@ function convexConvex(p1, p2) {
  *  {t:} if intersection
  *    -- NOTE: 0.0 <= t <= 1.0 gives the position of the first intersection
  */
-function rayCircle(r, c) {
-	//TODO
+function rayCircle(r, c)
+{
+	//This method uses the quadratic formula in combination with the equation of a circle.
+    var xDiff = r.end.x - r.start.x;
+    var yDiff = r.end.y - r.start.y;
+    var a = (xDiff * xDiff) + (yDiff * yDiff);
+    var b = (2 * xDiff * (r.start.x - c.x)) + (2 * yDiff * (r.start.y - c.y));
+    var c = ((r.start.x - c.x) * (r.start.x - c.x)) + ((r.start.y - c.y) * (r.start.y - c.y)) - (c.r * c.r);
+    
+    var dividend = -b - Math.sqrt((b * b) - (4 * a * c));
+    var divisor = 2 * a;
+    var t = dividend / divisor;
+    
+    if(t >= 0)
+    {
+        return {t: t};
+    }
+    
 	return null;
 }
 
@@ -55,8 +102,27 @@ function rayCircle(r, c) {
  *  {t:} if intersection
  *    -- NOTE: 0.0 <= t <= 1.0 gives the position of the first intersection
  */
-function rayRectangle(r, b) {
-	//TODO
+function rayRectangle(r, b)
+{
+	//Implementation of the algorithm found here: https://www.siggraph.org/education/materials/HyperGraph/raytrace/rtinter3.htm
+    var tOne = (b.min.x - r.start.x) / r.end.x;
+    var tTwo = (b.max.x - r.start.x) / r.end.x;
+    
+    if(tOne > tTwo)
+    {
+        var temp = tOne;
+        tOne = tTwo;
+        tTwo = temp;
+    }
+    
+    var tNear = tOne;
+    var tFar = tTwo;
+    
+    if( !(tNear > tFar || tFar < 0) )
+    {
+        return {t: tNear};
+    }
+    
 	return null;
 }
 
